648425713aa1db63fe13278ce373d19c360c5f2d
= <=>
2025-09-12 16:41:22 -0300

    Added Task routes

---
+++ src/API/Routes/TaskRoutes.ts
@@ -0,0 +1,51 @@
+import { Router } from "express";
+import { ITaskController } from "../Controllers/ITaskController";
+import { ITaskRoutes } from "./ITaskRoutes";
+
+export class TaskRoutes implements ITaskRoutes {
+    private Routes: Router;
+    private TaskController: ITaskController;
+
+    constructor(Routes: Router, TaskController: ITaskController) {
+        this.Routes = Routes;
+        this.TaskController = TaskController;
+        this.GetRoutes();
+        this.GetTaskRoutes();
+    }
+    public GetRoutes(): Router {
+        return this.Routes;
+    }
+
+    private GetTaskRoutes() {
+        this.Routes.post('/AddTask', async (req, res) => {
+            const task = req.body;
+            const createdTask = await this.TaskController.CreateTask(task);
+            res.status(201).json(createdTask);
+        });
+
+        this.Routes.get('/GetAllTasks', async (req, res) => {
+            const tasks = await this.TaskController.GetAllTasks();
+            res.status(200).json(tasks);
+        });
+        this.Routes.put('/UpdateTask/:id', async (req, res) => {
+            const { id } = req.params;
+            const updatedTaskData = req.body;
+            const updatedTask = await this.TaskController.UpdateTask(id, updatedTaskData);
+            if (updatedTask) {
+                res.status(200).json(updatedTask);
+            } else {
+                res.status(404).json({ message: "Task not found" });
+            }
+        });
+
+        this.Routes.delete('/DeleteTask/:id', async (req, res) => {
+            const { id } = req.params;
+            const isDeleted = await this.TaskController.DeleteTask(id);
+            if (isDeleted) {
+                res.status(204).send();
+            } else {
+                res.status(404).json({ message: "Task not found" });
+            }
+        });
+    }
+}
